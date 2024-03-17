c8d31eb454182f9cf4bec651be821d63bbfe20e2
JayeshYadav07 <jayeshyadav8080@gmail.com>
2024-03-17 12:49:04 +0530

    Step 5 - Add cors, body parser and jsonwebtoken

--- week-8/paytm/backend/routes/index.js
+++ week-8/paytm/backend/routes/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const userRouter = require("./user");
 const accountRouter = require("./account");
+
+router.use(cors());
+router.use(bodyParser.json());
 
 router.use("/user", userRouter);
 router.use("/account", accountRouter);
