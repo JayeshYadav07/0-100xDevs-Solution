dfb8f45671d9b16911382f7e1605548aa12979c5
JayeshYadav07 <jayeshyadav8080@gmail.com>
2024-03-17 12:42:35 +0530

    Step 3 - Create routing file structure

--- week-8/paytm/backend/index.js
+++ week-8/paytm/backend/index.js
@@ -1,3 +1,25 @@
 const express = require("express");
+const { db_connection } = require("./configs/db");
+const v1Router = require("./routes/index");
+require("dotenv").config();
 
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.use("/api/v1", v1Router);
+
+
+
+app.listen(PORT, async () => {
+	try {
+		await db_connection;
+		console.log("Connected to DB");
+	} catch (err) {
+		console.log(err);
+	}
+	console.log(`Server running on ${PORT} !`);
+});
---
+++ week-8/paytm/backend/routes/user.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+	res.send("This is the user request");
+});
+
+module.exports = router;
