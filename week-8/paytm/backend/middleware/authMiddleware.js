dbd318ab18342e767ee33baf57fdbf66ae0a70a1
JayeshYadav07 <jayeshyadav8080@gmail.com>
2024-03-17 15:05:29 +0530

    Step 7 - authMiddleware

---
+++ week-8/paytm/backend/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const JWT_SECRET = process.env.JWT_SECRET;
+const jwt = require("jsonwebtoken");
+
+function authMiddleware(req, res, next) {
+	const authHeader = req.headers.authorization;
+
+	if (!authHeader || !authHeader.startsWith("Bearer ")) {
+		return res.status(403).json({});
+	}
+
+	const token = authHeader.split(" ")[1];
+
+	try {
+		let decoded = jwt.verify(token, JWT_SECRET);
+		req.userId = decoded.userId;
+		next();
+	} catch (err) {
+		return res.status(401).send({ msg: err.message });
+	}
+}
+module.exports = authMiddleware;
--- week-8/paytm/backend/routes/account.js
+++ week-8/paytm/backend/routes/account.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/", (req, res) => {
 	res.send("This is the account request");
+});
+
+router.get("/test", authMiddleware, (req, res) => {
+	const userId = req.userId;
+	res.send({ message: "This is the test request", userId: userId });
 });
 
 module.exports = router;
