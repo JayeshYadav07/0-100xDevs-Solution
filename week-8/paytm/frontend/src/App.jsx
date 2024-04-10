34a28151766d8d76363031501fe05f6749977b12
JayeshYadav07 <jayeshyadav8080@gmail.com>
2024-04-10 12:38:58 +0530

    tailwind add and routes create

--- week-8/paytm/frontend/src/App.jsx
+++ week-8/paytm/frontend/src/App.jsx
@@ -1,11 +1,23 @@
-
-function App() {
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Send from "./pages/Send";
+import Dashboard from "./pages/Dashboard";
+import NoPage from "./pages/NoPage";
+export default function App() {
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<h1 className=" text-center font-bold">I am base Routes</h1>}
+        />
+        <Route path="Signin" element={<Signin />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="dashboard" element={<Send />} />
+        <Route path="send" element={<Dashboard />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
